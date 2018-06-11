5dbba27d950715769307f64e017f52569d18fe60
Dmitry Malinovsky <damalinov@gmail.com>
2018-06-11 23:25:34 +0600

    $GIT_TAG -> $TRAVIS_TAG

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-if [[ $GIT_TAG != "" ]] && [[ $TRAVIS_PYTHON_VERSION == "3.6" ]]; then
+if [[ $TRAVIS_TAG != "" ]] && [[ $TRAVIS_PYTHON_VERSION == "3.6" ]]; then
     poetry publish --no-interactive --build --username malinoff --password "$PYPI_PASSWORD"
 fi
