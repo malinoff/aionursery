6ed64f90802f80d3f8e2f83a362a897611f13916
Dmitry Malinovsky <damalinov@gmail.com>
2018-06-11 23:34:05 +0600

    Another attempt on publishing

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [[ $TRAVIS_TAG != "" ]] && [[ $TRAVIS_PYTHON_VERSION == "3.6" ]]; then
-    poetry publish --no-interactive --build --username malinoff --password "$PYPI_PASSWORD"
+    poetry publish --no-interaction --build --username malinoff --password "$PYPI_PASSWORD"
 fi
